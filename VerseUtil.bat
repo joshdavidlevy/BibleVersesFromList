ddfc34f168a762516e3a05fda1520e89217529e8
Josh Levy <joshmorrissey@protonmail.com>
2020-05-03 17:34:04 -0300

    changed json formatting

--- VerseUtil.bat
+++ VerseUtil.bat
@@ -9,10 +9,10 @@ set datestamp=%YYYY%%MM%%DD%
 set obsPath=%systemdrive%%homepath%\AppData\Roaming\obs-studio\basic\scenes\
 @echo on
 
-copy "%obsPath%OA__Facebook_Live.json" .
+copy "%obsPath%OA_-_Facebook_Live.json" .
 
-start /wait python3 VerseUtil.py
+start /wait "%systemdrive%%homepath%\AppData\Local\Programs\Python\Python38-32\python.exe" VerseUtil.py
 
-move OA__Facebook_Live.json OA__Facebook_Live_-_%datestamp%.json
+move OA_-_Facebook_Live.json OA__Facebook_Live_-_%datestamp%.json
 
 move "OA__Facebook_Live_-_%datestamp%.json" "%obsPath%"
--- VerseUtil.py
+++ VerseUtil.py
@@ -12,12 +12,50 @@ for book in bookData:
         books[abbrev] = book
 
 #Load Bible.json
-with open('Bible.json') as f:
-  bibleData = json.load(f)
-for book in bibleData:
-    if not book in bookData:
-        print("ERROR! No abbreviation found for " + book)
-        print("Please update Books.json")
+
+bibleData = {}
+
+with open('Bible.json') as json_file:
+    data = json.load(json_file)    
+    for bookData in data["XMLBIBLE"]["BIBLEBOOK"]:
+
+        book = bookData['_bname']
+        bibleData[book] = {}
+
+        if "_cnumber" in bookData["CHAPTER"]:
+
+            chapter = 1
+            bibleData[book][chapter] = {}
+
+            for verseData in bookData["CHAPTER"]["VERSE"]:
+
+                if "__text" in verseData:
+
+                    verseNum = verseData["_vnumber"]
+                    verseText = " ".join(verseData["__text"].split())
+
+                    bibleData[book][chapter][verseNum] = verseText
+                else:
+                    pass
+        else:
+
+            for chapterData in bookData["CHAPTER"]:
+              
+                chapter = chapterData['_cnumber']
+                bibleData[book][chapter] = {}
+
+                for verseData in chapterData["VERSE"]:
+
+                    if "__text" in verseData:
+
+                        verseNum = verseData["_vnumber"]
+                        verseText = " ".join(verseData["__text"].split())
+
+                        bibleData[book][chapter][verseNum] = verseText
+                    else:
+                        pass
+
+
     
 verses = []
 # Load verses.txt containing list of verses
@@ -27,32 +65,44 @@ with open(filepath) as fp:
     cnt = 1
     while line:
         if (line.strip()):
-            splitLine = line.split()
-            citation = splitLine.pop()
 
-            book = ' '.join(splitLine).strip()
-
-            try:
-                book = books[book]
-            except:
-                print("ERROR: Book not found: " + book)
+            customLine = line.split("|")
+            if len(customLine) > 1:
+                verses.append({"citation":customLine[0].strip(), "text":customLine[1].strip()})
+            else:
             
-            chapter = citation.split(":")[0]
-            startVerse = citation.split(":")[1].split("-")[0]
-            endVerse = None
-            try:
-                endVerse = citation.split(":")[1].split("-")[1]
-            except:
-                endVerse = startVerse
+                splitLine = line.split()
+                citation = splitLine.pop()
 
-            for verse in range(int(startVerse), int(endVerse)+1):
-                text = ""
+                book = ' '.join(splitLine).strip()
+
                 try:
-                    text = bibleData[book][chapter][str(verse)]
+                    book = books[book]
                 except:
-                    print("ERROR: Unable to find " + book + " " + chapter + ":" + str(verse))
+                    print("ERROR: Book not found: " + book)
                 
-                verses.append({"citation": book + " " + chapter + ":" + str(verse), "text":text})     
+                chapter = citation.split(":")[0]
+
+                startVerse = citation.split(":")[1].replace(",","-").split("-")[0]
+                endVerse = None
+                try:
+                    endVerse = citation.split(":")[1].split("-")[1]
+                except:
+                    endVerse = startVerse
+
+                for verse in range(int(startVerse), int(endVerse)+1):
+                    text = ""
+                    try:
+                        text = bibleData[book][chapter][str(verse)]
+                    except:
+                        try:
+                            # Single chapter books
+                            text = bibleData[book][1][str(verse)]
+                        except:
+                            print("ERROR: Unable to find " + book + " " + chapter + ":" + str(verse))
+                        
+                    
+                    verses.append({"citation": book + " " + chapter + ":" + str(verse), "text":text})     
 
         line = fp.readline()
         cnt += 1
@@ -71,7 +121,7 @@ for verse in verses:
 
     #Optionally, replace the verse source names with their citations in the OBS profile
     try:
-        with fileinput.FileInput("OA__Facebook_Live.json", inplace=True) as file:
+        with fileinput.FileInput("OA_-_Facebook_Live.json", inplace=True) as file:
             for line in file:
                 print(line.replace('"Verse ' + str(num) + '"', '"' + verse["citation"] + '"'), end='')
     except:
@@ -85,7 +135,7 @@ dateStamp = d.strftime('%Y-%m-%d')
 
 #Optionally, if we replaced the verse headings above, update the date stamp so as not to overwrite the template
 try:    
-    with fileinput.FileInput("OA__Facebook_Live.json", inplace=True) as file:
+    with fileinput.FileInput("OA_-_Facebook_Live.json", inplace=True) as file:
         for line in file:
             print(line.replace('OA - Facebook Live', 'OA - Facebook Live - ' + dateStamp))
 except:
